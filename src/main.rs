8697c81efe496735eb1453f50614b5d9b078f133
Farooq Karimi Zadeh <fkz@riseup.net>
2023-04-10 14:58:12 +0330

    InIt

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+use anyhow::{Context as _, Result};
+use deltachat::config::Config;
+use deltachat::chat::{self, Chat, ChatId};
+use deltachat::context::{Context, ContextBuilder};
+use deltachat::message::{Message, MsgId, Viewtype};
+use deltachat::EventType;
+
+use serde::Deserialize;
+use std::env::{args, current_dir};
+use std::fs::read;
+use std::path::{Path, PathBuf};
+use std::str::from_utf8;
+
+#[derive(Deserialize)]
+struct BotConfig {
+    email: String,
+    password: String,
+    db: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let botconfig: BotConfig;
+    {
+        let mut config_file_path = current_dir()
+            .expect("Cannot get current directory")
+            .join("config.toml");
+        if let Some(arg) = args().nth(1) {
+            config_file_path = PathBuf::from(arg);
+        }
+        botconfig = toml::from_str(from_utf8(&read(config_file_path)?)?)?;
+    }
+
+    println!(
+        "Starting the bot. Db: {} Address: {}",
+        botconfig.db, botconfig.email
+    );
+    let ctx = ContextBuilder::new(botconfig.db.into())
+        .open()
+        .await
+        .context("Creating context failed")?;
+    let events_emitter = ctx.get_event_emitter();
+    let emitter_ctx = ctx.clone();
+    tokio::spawn(async move {
+        while let Some(event) = events_emitter.recv().await {
+            match event.typ {
+                EventType::IncomingMsg { chat_id, msg_id } => {
+                    if let Err(err) = handle_message(&emitter_ctx, chat_id, msg_id).await {
+                        println!("error handling message: {err}");
+                    }
+                }
+                _ => {}
+            }
+        }
+    });
+    
+    if !ctx.get_config_bool(Config::Configured).await? {
+        ctx.set_config(Config::Addr, Some(&botconfig.email)).await?;
+        ctx.set_config(Config::MailPw, Some(&botconfig.password)).await?;
+        ctx.set_config(Config::Bot, Some("1")).await?;
+        ctx.set_config(Config::E2eeEnabled, Some("1")).await?;
+        ctx.configure().await.context("configuration failed...")?;
+    }
+
+    ctx.start_io().await;
+    tokio::signal::ctrl_c().await?;
+    ctx.stop_io().await;
+    Ok(())
+}
+
+async fn handle_message(ctx: &Context, chat_id: ChatId, msg_id: MsgId) -> anyhow::Result<()> {
+    let chat = Chat::load_from_db(ctx, chat_id).await?;
+    let incoming_msg = Message::load_from_db(ctx, msg_id)
+        .await?
+        .get_text();
+
+    let mut msg = Message::new(Viewtype::Text);
+    msg.set_text(incoming_msg);
+    println!("Sending back a message...");
+    chat::send_msg(ctx, chat_id, &mut msg).await?;
+    Ok(())
+}
