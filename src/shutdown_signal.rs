02f81e21bd18b275d6d8e03c3261bddf3df77f9e
Simon Laux <mobile.info@simonlaux.de>
2023-05-29 10:37:03 +0200

    working shutdown

---
+++ src/shutdown_signal.rs
@@ -0,0 +1,28 @@
+use tokio::signal;
+
+// adopted from https://github.com/tokio-rs/axum/blob/main/examples/graceful-shutdown/src/main.rs
+pub(crate) async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    log::info!("signal received, starting graceful shutdown");
+}
